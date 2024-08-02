6eeb738d01e1c84de1372585cdc14fb25692c375
rohnshrma <rohn11shrma@gmail.com>
2024-08-02 14:19:29 +0530

    userplaces and button

--- src/places/Components/PlaceItem.js
+++ src/places/Components/PlaceItem.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Card from "../../Shared/components/UiElements/Card";
+import Button from "../../Shared/components/UiElements/Button";
 
-const PlaceItem = () => {
-  return <div>PlaceItem</div>;
+const PlaceItem = (props) => {
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
+        <div className="place-item__image">
+          <img src={props.image} alt={props.title} />
+        </div>
+
+        <div className="place-item__info">
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </div>
+
+        <div className="place-item__actions">
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${props.id}`}>EDIT</Button>
+          <Button>DELETE</Button>
+        </div>
+      </Card>
+    </li>
+  );
 };
 
 export default PlaceItem;
--- src/places/Pages/UserPlaces.js
+++ src/places/Pages/UserPlaces.js
@@ -1,47 +1,86 @@
-import React, { useEffect, useState } from "react";
-import { useHttpClient } from "../../Shared/Hooks/http-hook";
-import { useParams } from "react-router-dom";
-import LoadingSpinner from "../../Shared/components/UiElements/LoadingSpinner";
+// import React, { useEffect, useState } from "react";
+// import { useHttpClient } from "../../Shared/Hooks/http-hook";
+// import { useParams } from "react-router-dom";
+// import LoadingSpinner from "../../Shared/components/UiElements/LoadingSpinner";
+// import PlaceList from "../Components/PlaceList";
+// const UserPlaces = () => {
+//   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+//   const [loadedPlaces, setLoadedPlaces] = useState();
+
+//   const userId = useParams().uid;
+
+//   console.log(useParams().uid);
+
+//   useEffect(() => {
+//     const fetchPlaces = async () => {
+//       try {
+//         const responseData = await sendRequest(
+//           `http://localhost:5000/api/places/user/${userId}`
+//         );
+//         setLoadedPlaces(responseData.places);
+//       } catch (err) {}
+//     };
+//     fetchPlaces();
+//   }, [userId, sendRequest]);
+
+//   const placeDeletedHandler = (deletedPlaceId) => {
+//     setLoadedPlaces((prevPlaces) => {
+//       return prevPlaces.filter((place) => place.id !== deletedPlaceId);
+//     });
+//   };
+
+//   return (
+//     <>
+//       {/* error modal to be added later */}
+
+//       {isLoading && (
+//         <div className="center">
+//           <LoadingSpinner />
+//         </div>
+//       )}
+//       {!isLoading && loadedPlaces && (
+//         <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
+//       )}
+//     </>
+//   );
+// };
+
+// export default UserPlaces;
+
+import React from "react";
 import PlaceList from "../Components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
 const UserPlaces = () => {
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
-
-  const [loadedPlaces, setLoadedPlaces] = useState();
-
-  const userId = useParams().userId;
-
-  useEffect(() => {
-    const fetchPlaces = async () => {
-      try {
-        const responseData = await sendRequest(
-          `http://localhost:5000/api/places/user/${userId}`
-        );
-        setLoadedPlaces(responseData.places);
-      } catch (err) {}
-    };
-    fetchPlaces();
-  }, [userId, sendRequest]);
-
-  const placeDeletedHandler = (deletedPlaceId) => {
-    setLoadedPlaces((prevPlaces) => {
-      return prevPlaces.filter((place) => place.id !== deletedPlaceId);
-    });
-  };
-
-  return (
-    <>
-      {/* error modal to be added later */}
-
-      {isLoading && (
-        <div className="center">
-          <LoadingSpinner />
-        </div>
-      )}
-      {!isLoading && loadedPlaces && (
-        <PlaceList items={loadedPlaces} onDeletePlace={placeDeletedHandler} />
-      )}
-    </>
-  );
+  return <PlaceList items={DUMMY_PLACES} />;
 };
 
 export default UserPlaces;
